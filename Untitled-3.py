9b19987628050fac7eb341098cce66944ccb4afd
below0iq <cherevatenkovlad1@gmail.com>
2024-12-15 15:38:04 +0300

    Update Untitled-3.py

--- Untitled-3.py
+++ Untitled-3.py
@@ -2,59 +2,68 @@ import os
 import requests
 from urllib.parse import urlparse
 
+
 def count_clicks(token, short_link):
-    vk_linkstats="https://api.vk.ru/method/utils.getLinkStats"
+    url = "https://api.vk.ru/method/utils.getLinkStats"
     parameters = { 
-        "access_token":token,
-        "v":5.199,
-        "key":urlparse(short_link).path[1:],
-        "interval":"forever",
-        "extended":0
+        "access_token": token,
+        "v": 5.199,
+        "key": urlparse(short_link).path[1:],
+        "interval": "forever",
+        "extended": 0
     }
-    response = requests.get(vk_linkstats, params=parameters)
+    response = requests.get(url, params=parameters)
     response.raise_for_status()
-    return response.json()["response"]["stats"][0]["views"]
+    return response.json()["response"]["stats"]
+
 
 def shorten_link(token, url):
-    vk_shortlink="https://api.vk.ru/method/utils.getShortLink"
+    url = "https://api.vk.ru/method/utils.getShortLink"
     parameters = {
-        "access_token":token,
-        "v":5.199,
-        "url":url
+        "access_token": token,
+        "v": 5.199,
+        "url": url
     }
-    response = requests.get(vk_shortlink, params=parameters)
+    response = requests.get(url, params=parameters)
+    response.raise_for_status()
     return response.json()["response"]["short_url"]
 
+
 def is_shorten_link(url, token):
-    vk_linkstats="https://api.vk.ru/method/utils.getLinkStats"
+    url = "https://api.vk.ru/method/utils.getLinkStats"
     parameters = { 
-        "access_token":token,
-        "v":5.199,
-        "key":urlparse(url).path[1:],
-        "interval":"forever",
-        "extended":0
+        "access_token": token,
+        "v": 5.199,
+        "key": urlparse(url).path[1:],
+        "interval": "forever",
+        "extended": 0
     }
-    response = requests.get(vk_linkstats, params=parameters)
+    response = requests.get(url, params=parameters)
     response.raise_for_status()
     try:
-        response.json()["response"]["stats"][0]["views"]
+        response.json()["response"]["stats"]
         return True
     except:
         return False
 
+
 def main():
-    url=input("Введите ссылку: ")
-    token=os.environ["TOKEN"]
+    url = input("Введите ссылку: ")
+    token = os.environ["TOKEN"]
     if is_shorten_link(url, token):
         try:
-            print(count_clicks(token, url))
-        except: 
-            print("Произошла ошибка")
+            if len(count_clicks(token, url)) == 0:
+                print("По ссылке еще не переходили")
+            elif count_clicks(token, url)[0]["views"] > 0:
+                print(f"По ссылке перешли {count_clicks(token, url)[0]["views"]} раз")
+        except:
+            print("Неправильный ввод ссылки")
     else:
         try:
             print(shorten_link(token, url))
-        except: 
-            print("Произошла ошибка")
+        except KeyError: 
+            print("Неправильный ввод ссылки")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
